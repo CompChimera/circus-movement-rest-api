57e22c38772f346427beab9963b1afe63d6a782a
Brittany Kelly <computationalchimera@gmail.com>
2024-03-06 00:32:25 -0400

    Outline update move endpoint. Cleanup

--- resources/move.py
+++ resources/move.py
@@ -8,7 +8,6 @@ from schemas import MoveSchema, MoveUpdateSchema, RoutineAndMoveSchema
 
 blp = Blueprint("moves", __name__, description="Operations on moves")
 
-# Get or Create Apparatus Move
 @blp.route("/apparatus/<string:apparatus_id>/move")
 class AddMovesForApparatus(MethodView):
     @blp.response(200, MoveSchema(many=True))
@@ -38,7 +37,6 @@ class MovesForApparatus(MethodView):
         move = MoveModel.query.get_or_404(move_id)
         return move
 
-    # @blp.response(200, ApparatusModel)
     def delete(self, apparatus_id, move_id):
         apparatus = ApparatusModel.query.get_or_404(apparatus_id)
         move = MoveModel.query.get_or_404(move_id)
@@ -49,11 +47,21 @@ class MovesForApparatus(MethodView):
             db.session.add(apparatus)
             db.session.delete(move)
             db.session.commit()
-        except SQLAlchemyError as e:
-            abort(500, message=e)
-            # abort(500, message="An error occured while removing the move from the apparatus")
+        except SQLAlchemyError:
+            abort(500, message="An error occured while removing the move from the apparatus")
 
         return {"message":"Move removed from Apparatus", "apparatus": apparatus.name}
+    
+    @blp.arguments(MoveUpdateSchema)
+    @blp.response(200, MoveSchema)
+    def put(self, move_data, apparatus_id, move_id):
+        move = MoveModel.query.get(move_id)
+
+        if move_id:
+            move.name = move_data["name"]
+        else:
+            move = MoveModel(id=move_id, **move_data) 
+        raise NotImplementedError("Updating an apparatus move is not yet implemented.")
 
 @blp.route("/routine/<string:routine_id>/move/<string:move_id>")
 class LinkMovesToRoutine(MethodView):
@@ -98,5 +106,3 @@ class MoveList(MethodView):
     @blp.response(200, MoveSchema(many=True))
     def get(self):
         return MoveModel.query.all()
-
-    
